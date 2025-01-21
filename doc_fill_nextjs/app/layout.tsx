d10fae45b2efc5880c15f1f5f7dd66fa25abc771
naiden00 <naiden.petrov.31.12.00@gmail.com>
2025-01-21 18:32:27 +0200

    approuter

--- doc_fill_nextjs/app/layout.tsx
+++ doc_fill_nextjs/app/layout.tsx
@@ -1,11 +1,19 @@
+import theme from "@/config/theme";
+import { ThemeProvider } from "@mui/material";
+import { AppRouterCacheProvider } from "@mui/material-nextjs/v15-appRouter";
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="en" suppressHydrationWarning>
+      <body>
+        <AppRouterCacheProvider>
+          <ThemeProvider theme={theme}>{children}</ThemeProvider>
+        </AppRouterCacheProvider>
+      </body>
     </html>
   );
 }
--- doc_fill_nextjs/app/page.tsx
+++ doc_fill_nextjs/app/page.tsx
@@ -1,33 +1,62 @@
-import AcmeLogo from '@/app/ui/acme-logo';
-import { ArrowRightIcon } from '@heroicons/react/24/outline';
-import Link from 'next/link';
+"use client";
 
-export default function Page() {
+import { useTheme, Box, Typography, ButtonBase } from "@mui/material";
+import Image from "next/image";
+
+const HomePage = () => {
+  const theme = useTheme();
+
   return (
-    <main className="flex min-h-screen flex-col p-6">
-      <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
-        {/* <AcmeLogo /> */}
-      </div>
-      <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
-        <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
-          <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal`}>
-            <strong>Welcome to Acme.</strong> This is the example for the{' '}
-            <a href="https://nextjs.org/learn/" className="text-blue-500">
-              Next.js Learn Course
-            </a>
-            , brought to you by Vercel.
-          </p>
-          <Link
-            href="/login"
-            className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
+    <Box
+      component="main"
+      sx={{
+        position: "absolute",
+        width: "100%",
+        height: "100%",
+        top: 0,
+        left: 0,
+      }}
+    >
+      <Image
+        src={"/IMG_2990.webp"}
+        alt="Background Image"
+        fill
+        priority
+        style={{ zIndex: -10 }}
+      />
+      <Box sx={{ display: "flex", justifyContent: "center", pt: 24 }}>
+        <Typography variant="h1" fontFamily={"cursive"}>
+          ЧСИ НЕДЕЛЧО МИТЕВ
+        </Typography>
+      </Box>
+      <Box sx={{ display: "flex", justifyContent: "center", mt: 24 }}>
+        <ButtonBase
+          sx={{
+            width: 320,
+            height: 160,
+            textDecoration: "none",
+            backgroundColor: theme.palette.secondary.main,
+            borderRadius: 8,
+            textAlign: "center",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            "&:hover": {
+              backgroundColor: theme.palette.secondary.light, // Optional hover effect
+            },
+          }}
+        >
+          <Typography
+            variant="h4"
+            fontFamily={"sans-serif"}
+            color="textPrimary"
           >
-            <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
-          </Link>
-        </div>
-        <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
-          {/* Add Hero Images Here */}
-        </div>
-      </div>
-    </main>
+            ПОПЪЛНИ ДОКУМЕНТИ
+          </Typography>
+        </ButtonBase>
+      </Box>
+    </Box>
   );
-}
+};
+
+export default HomePage;
